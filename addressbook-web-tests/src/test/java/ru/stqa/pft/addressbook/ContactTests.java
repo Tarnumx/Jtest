8c9d4232fe651a9c963e14e675f2bcb59efcb635
Tarnumx <simmons@yandex.ru>
2016-12-14 19:40:32 +0300

    groupCreation

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactTests.java
@@ -0,0 +1,88 @@
+package ru.stqa.pft.addressbook;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+
+public class ContactTests {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/");
+        login("admin", "secret");
+    }
+
+    private void login(String userName, String password) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(userName);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
+    @Test
+    public void testContact() {
+
+
+        groupCreation();
+        groupForms(new FormGroups("Alexey", "Sitnikov", "Moscow", "988", "alexey.sitnikov@ya.ru"));
+        enterInfo();
+        returnToHomepage();
+    }
+
+    private void returnToHomepage() {
+        wd.findElement(By.linkText("home page")).click();
+    }
+
+    private void enterInfo() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    private void groupForms(FormGroups formGroups) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(formGroups.getFirstName());
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(formGroups.getLastName());
+        wd.findElement(By.name("address")).click();
+        wd.findElement(By.name("address")).clear();
+        wd.findElement(By.name("address")).sendKeys(formGroups.getAddress());
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).clear();
+        wd.findElement(By.name("mobile")).sendKeys(formGroups.getPhoneNumMob());
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(formGroups.getEmail());
+    }
+
+    private void groupCreation() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/FormGroups.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class FormGroups {
+  private final String firstName;
+  private final String lastName;
+  private final String address;
+  private final String phoneNumMob;
+  private final String email;
+
+  public FormGroups(String firstName, String lastName, String address, String phoneNumMob, String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.phoneNumMob = phoneNumMob;
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getPhoneNumMob() {
+    return phoneNumMob;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
